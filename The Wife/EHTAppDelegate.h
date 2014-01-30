1cca1a3ab76fc155b33b7b1401ccfdc563a3d5f7
MorganCabral <morgan.cabral@gmail.com>
2014-01-30 17:46:34 -0500

    Initial Commit

---
+++ The Wife/EHTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EHTAppDelegate.h
+//  The Wife
+//
+//  Created by Morgan Cabral on 1/30/14.
+//  Copyright (c) 2014 RIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EHTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ The Wife/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  The Wife
+//
+//  Created by Morgan Cabral on 1/30/14.
+//  Copyright (c) 2014 RIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EHTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EHTAppDelegate class]));
+    }
+}

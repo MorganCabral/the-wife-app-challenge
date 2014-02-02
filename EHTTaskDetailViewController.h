fd2b603ec2188e369a6aca53ff8e1e8c7ddc3e65
MorganCabral <morgan.cabral@gmail.com>
2014-02-02 00:36:02 -0500

    First stab at a storyboard for the Task Preview screen.

---
+++ EHTTaskDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  EHTTaskDetailViewController.h
+//  The Wife
+//
+//  Created by Morgan Cabral on 2/1/14.
+//  Copyright (c) 2014 RIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EHTTaskDetailViewController : UIViewController
+
+@end
